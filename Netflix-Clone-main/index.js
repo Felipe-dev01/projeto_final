951ed7d6977e0115b21c7f08647822c0fae74c72
Felipe Farias <161083885+Felipe-dev01@users.noreply.github.com>
2024-11-28 23:15:42 +0000

    atualizaçao do js e index

--- Netflix-Clone-main/index.js
+++ Netflix-Clone-main/index.js
@@ -7,24 +7,22 @@ const imgPath = "https://image.tmdb.org/t/p/original";
 const apiPaths = {
     buscarTodasCategorias: `${apiEndpoint}/genre/movie/list?api_key=${apikey}`,
     buscarListaFilmes: (id) => `${apiEndpoint}/discover/movie?api_key=${apikey}&with_genres=${id}`,
-    buscarTrending:`${apiEndpoint}/trending/all/day?api_key=${apikey}&language=en-US`,
+    buscarTrending:`${apiEndpoint}/trending/all/day?api_key=${apikey}&language=pt-BR`,
 }
 
 
 // Inicializa o app
 function inicializar() {
-    buscarFilmesTrending();
+    buscarFilmesCarrossel();
     buscarEConstruirTodasSecoes();
 }
 
-function buscarFilmesTrending(){
+function buscarFilmesCarrossel(){
     buscarEConstruirSecaoFilmes(apiPaths.buscarTrending, 'Trending Now')
     .then(lista => {
         const indiceAleatorio = parseInt(Math.random() * lista.length);
         construirSecaoBanner(lista[indiceAleatorio]);
-    }).catch(err=>{
-        console.error(err);
-    });
+    })
 }
 
 function construirSecaoBanner(filme){
@@ -36,11 +34,10 @@ function construirSecaoBanner(filme){
 
     div.innerHTML = `
             <h2 class="banner__title">${filme.title}</h2>
-            <p class="banner__info">Trending in movies | Released - ${filme.release_date} </p>
+            <p class="banner__info">Filmes em alta | Data de Lançamento - ${filme.release_date} </p>
             <p class="banner__overview">${filme.overview && filme.overview.length > 200 ? filme.overview.slice(0,200).trim()+ '...':filme.overview}</p>
             <div class="action-buttons-cont">
-                <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path></svg> &nbsp;&nbsp; Play</button>
-                <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3ZM1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z" fill="currentColor"></path></svg> &nbsp;&nbsp; More Info</button>
+                <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3ZM1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z" fill="currentColor"></path></svg> &nbsp;&nbsp; Mais Informações</button>
             </div>
         `;
     div.className = "banner-content container";
